a1a6ebe30d16db07c652c6ddd14d5a782cc49aae
FOSSEnjoyer69 <129515422+FOSSEnjoyer69@users.noreply.github.com>
2023-03-31 22:18:17 +1000

    Add files via upload

---
+++ ChangeFolderVideoFilesResolution.py
@@ -0,0 +1,31 @@
+import os
+from moviepy.editor import *
+
+resolution = 1920, 1080
+
+sourceFolder = "E:\Shows\Wednesday (2022)\Episode 4 Woe What a Night\Clips"
+targetFolder = f"{sourceFolder}/{resolution[0]} x {resolution[1]}"
+
+# Cache the files to avoid regenerating them on subsequent runs
+files = []
+for root, directories, filenames in os.walk(sourceFolder):
+    for filename in filenames:
+        files.append(os.path.join(root, filename))
+
+if not os.path.exists(targetFolder):
+    os.makedirs(targetFolder)
+
+for file_path in files:
+    # Get the file name
+    file_name = os.path.basename(file_path)
+    resized_file_path = os.path.join(targetFolder, file_name)
+
+    if not os.path.exists(resized_file_path):
+        # Load the video
+        clip = VideoFileClip(file_path)
+
+        # Resize the video
+        clip_resized = clip.resize(resolution)
+
+        # Save the resized video to a new file
+        clip_resized.write_videofile(resized_file_path, codec='libx264')
